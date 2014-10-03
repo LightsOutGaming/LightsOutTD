12a06301352243694105e2c4f34985c8d6177a3d
taz40 <ldspcjunkie@gmail.com>
2014-10-02 19:32:40 -0600

    things

--- src/io/brace/lightsoutgaming/towerdefence/Main.java
+++ src/io/brace/lightsoutgaming/towerdefence/Main.java
@@ -1,5 +1,32 @@
 package io.brace.lightsoutgaming.towerdefence;
 
-public class Main {
+import io.brace.lightsoutgaming.engine.LightsOut;
+
+public class Main extends LightsOut {
+	
+	public static void main(String[] args){
+		new Main().init();
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		this.createDisplay("Lights Out Tower Defence", 900, 600);
+		this.start();
+	}
+
+	@Override
+	protected void render() {
+		// TODO Auto-generated method stub
+		screen.clear();
+		
+		show();
+	}
+
+	@Override
+	protected void update() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
